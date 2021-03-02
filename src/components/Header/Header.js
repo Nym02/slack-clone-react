d044bf3f350ade9f1343035829b044ba512458eb
Nayeem M. Muzahid <nayeem.muzahid@northsouth.edu>
2021-03-03 04:59:56 +0600

    completed the header section

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,6 +2,8 @@ import React from "react";
 import styled from "styled-components";
 import { Avatar } from "@material-ui/core";
 import AccessTimeIcon from "@material-ui/icons/AccessTime";
+import SearchIcon from "@material-ui/icons/Search";
+import HelpOutlineIcon from "@material-ui/icons/HelpOutline";
 
 const Header = () => {
   return (
@@ -14,8 +16,16 @@ const Header = () => {
         <AccessTimeIcon />
       </HeaderLeft>
       {/* Header Search */}
+      <HeaderSearch>
+        <SearchIcon />
+        <input type="text" placeholder="Search anything...." />
+      </HeaderSearch>
 
       {/* Header Right  */}
+
+      <HeaderRight>
+        <HelpOutlineIcon />
+      </HeaderRight>
     </HeaderContainer>
   );
 };
@@ -47,5 +57,36 @@ const HeaderAvatar = styled(Avatar)`
   cursor: pointer;
   :hover {
     opacity: 0.8;
+  }
+`;
+const HeaderSearch = styled.div`
+  flex: 0.4;
+  opacity: 1;
+  border-radius: 6px;
+  background-color: #421f44;
+  text-align: center;
+  display: flex;
+  padding: 0 50px;
+  color: gray;
+  border: 1px gray solid;
+
+  > input {
+    background-color: transparent;
+    border: none;
+    text-align: center;
+    min-width: 30vw;
+    outline: none;
+    color: white;
+  }
+`;
+
+const HeaderRight = styled.div`
+  flex: 0.3;
+  display: flex;
+  align-items: flex-end;
+
+  > .MuiSvgIcon-root {
+    margin-left: auto;
+    margin-right: 20px;
   }
 `;
