bfa4d1ae775104b5db02b4f9c5432b82f8ee6f50
Nayeem M. Muzahid <nayeem.muzahid@northsouth.edu>
2021-03-03 04:42:21 +0600

    working on the header section

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Header from "./components/Header/Header";
 
 function App() {
   return (
     <div className="app">
-      <h1>Let's build slack</h1>
-
       <Router>
         <Switch>
-          <Route path="/"></Route>
-          <Route path="/"></Route>
-          <Route path="/"></Route>
+          <Route path="/" exact>
+            <Header></Header>
+          </Route>
         </Switch>
       </Router>
     </div>
