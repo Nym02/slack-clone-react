d1f1e0001f9020a4edd5b920d9c7391f640b635d
Nayeem M. Muzahid <nayeem.muzahid@northsouth.edu>
2021-03-02 20:34:09 +0600

    working on the header component

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBaVZU9n7751Liiqzy3GuNtLyikxf5u2wY",
+  authDomain: "slack-clone-react-feeeb.firebaseapp.com",
+  projectId: "slack-clone-react-feeeb",
+  storageBucket: "slack-clone-react-feeeb.appspot.com",
+  messagingSenderId: "321353427056",
+  appId: "1:321353427056:web:f845fddec367b08980acb5",
+};
